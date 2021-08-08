17db38c9ca5ac8ab636bf0cd2f96fd1084a828e6
Yuri Fontella <yurifc4@gmail.com>
2021-08-08 00:37:53 -0300

    add rule

--- src/app/controller/gql/user/query.resolvers.js
+++ src/app/controller/gql/user/query.resolvers.js
@@ -1,9 +1,13 @@
 'use strict'
 
+const { resolve } = require('path')
+
+const rule = require(resolve('src/app/rule/user'))
+
 module.exports = {
   Query: {
-    users: async (parent, args, { app }) => {
-      return await app.query.users()
+    users: (parent, args, { app }) => {
+      return rule.users(app.query)
     }
   }
 }
---
+++ src/app/rule/user/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+
+  users: (query) => {
+    return query.users()
+  }
+}
