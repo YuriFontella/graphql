8de541cbdf16942ffd7e821fd153f2a86cc91931
Yuri Fontella <yurifc4@gmail.com>
2021-08-09 00:12:19 -0300

    knex

--- src/infra/plugin/knex.js
+++ src/infra/plugin/knex.js
@@ -2,7 +2,9 @@
 
 const fp = require('fastify-plugin')
 
-const knexfile = require('../../../config/knexfile')
+const { resolve } = require('path')
+
+const knexfile = require(resolve('config/knexfile'))
 
 module.exports = fp(async (app) => {
   app.register(require('fastify-knexjs'), knexfile[process.env.NODE_ENV], err => console.error(err))
